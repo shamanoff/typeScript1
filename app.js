4bffba76f4eaaaddc30ac576f6b3c6f0e0cc03eb
Michael <shamanoff@gmail.com>
2016-10-29 22:51:24 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var isDone = false;
+var decimal = 6;
+var hex = 0xf00d;
+var city = "sts";
+var name1 = "bob";
+name1 = "smet";
+var list = [1, 2, 3];
+var listTuple;
+listTuple = ["vasia", 30];
+console.log(listTuple[0].substr(1));
+// console.log(listTuple[1].substr(1));
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,21 @@
+
+let isDone: boolean = false;
+let decimal: number =6;
+let hex: number = 0xf00d;
+
+let city:string = "sts";
+let name1: string = "bob";
+
+name1 = "smet";
+
+let list:number[] = [1, 2, 3];
+
+let listTuple: [string, number];
+listTuple = ["vasia", 30];
+
+console.log(listTuple[0].substr(1));
+// console.log(listTuple[1].substr(1));
+
+
+
+
